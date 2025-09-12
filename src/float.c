5d9f767f0c127ebaf9d0eeb1d7a484f7cfbd6e71
Andersen <andersen.ngai@stonybrook.edu>
2025-09-12 21:13:12 +0000

    Finished integer.c and have no idea what's happening in float.c

--- src/float.c
+++ src/float.c
@@ -6,17 +6,14 @@ float construct_float_sf(char sign_bit, char exponent, unsigned int fraction) {
     /* DO NOT CHANGE THE DECLARATION OF "f" (This will be converted to float later) */
     unsigned int f = 0; // DO NOT CHANGE
     /*------------------------------------------------------------------------------*/
-    
-    /* Start Coding Here */
 
-    
-    
-    
-    
-    
-    
-    /*-------------------*/
-    
+
+    f |= sign_bit;
+    f <<= 8;
+    f |= exponent;
+    f <<= 23;
+    f |= fraction;
+
     /* DO NOT CHANGE THE RETURN VALUE (This returns the binary representation of "f" as float) */
     return *((float*)&f); // DO NOT CHANGE
     /* ----------------------------------------------------------------------------------------*/
@@ -37,7 +34,19 @@ int main(int argc, char* argv[]){
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
 
+    construct_float_sf(0x01, 0x81, 0x300000); // -5.5
 
+    /*
+    printf("%d\n", construct_float_sf(0x00, 0x81, 0x300000)); // 5.5
+    printf("%d\n", construct_float_sf(0x01, 0x81, 0x300000)); // -5.5
+    printf("%d\n", construct_float_sf(0x00, 0x7F, 0x200000)); // 1.25
+    printf("%d\n", construct_float_sf(0x01, 0x7F, 0x200000)); // -1.25
+    printf("%d\n", construct_float_sf(0x00, 0x76, 0x299B6F)); // 0.002588
+    printf("%d\n", construct_float_sf(0x01, 0x76, 0x299B6F)); // -0.002588
+    printf("%d\n", construct_float_sf(0x00, 0x89, 0xABCDEF)); // 1374.44
+    printf("%d\n", construct_float_sf(0x01, 0x89, 0xABCDEF)); // -1374.44
+    printf("%d\n", construct_float_sf(0x00, 0x90, 0x7973C0)); // 255439
+    printf("%d\n", construct_float_sf(0x01, 0x90, 0x7973C0)); // -255439 */
 
 	
 	/** ---------------------- **/
--- src/integer.c
+++ src/integer.c
@@ -5,10 +5,47 @@
 // FILL IN THE BODY OF THIS FUNCTION.
 // Feel free to create any other functions you like; just include them in this file.
 
+void printBits(unsigned int repr){ // Just here for reference
+	for (int i = 31; i >= 0; i--)
+		printf("%d", (repr & (1 << i)) >> i);
+	printf("\n");
+}
 
+void asHex(unsigned int repr){
+	for (int i = 7; i >= 0; i--){
+		printf("%x", (repr & (0x0000000f << (i * 4))) >> (i * 4));
+	}
+	printf("\n");
+}
 
 void repr_convert(char source_repr, char target_repr, unsigned int repr) {
-
+	if (((source_repr != '2') & (source_repr != 'S')) || ((target_repr != '2') & (target_repr != 'S'))){
+		printf("error\n");
+		return;
+	}
+	if (source_repr == target_repr){
+		asHex(repr);
+		return;
+	}
+	if (source_repr == 'S'){ // Convert from sign/magnitude to 2's complement
+		if (repr & 0x80000000){ // Negative
+			// The MSB 1 is just a sign, not magnitude. We will take care of it.
+			// Flip all the bits except for the MSB. We will manually make it 1
+			// And then add 1
+			repr = ((~repr) | 0x80000000) + 1;
+		}
+		asHex(repr);
+		return;
+	}
+	// The only other possible case is conversion from 2's comp to sign/mag
+	if (repr == 0x80000000){
+		printf("undefined\n");
+		return;
+	}
+	if (repr & 0x80000000){ // Negative
+		repr = ((~repr) + 1) | 0x80000000;
+	}
+	asHex(repr);
 }
 
 
@@ -25,7 +62,14 @@ int main(int argc, char* argv[]){
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
 
-
+	repr_convert('S', '2', 0x80000001); // ffffffff
+	repr_convert('S', '2', 0x80000000); // 00000000
+	repr_convert('S', '2', 0x0fffffff); // 0fffffff // This is my own test case
+	repr_convert('2', '2', 0x59f2ca50); // 59f2ca50
+	repr_convert('2', '2', 0x00394812); // error
+	repr_convert('2', 'S', 0x80000000); // undefined
+	repr_convert('2', 'S', 0x80000010); // fffffff0
+	repr_convert('2', 'S', 0x70000010); // 70000010
 
 	
 	/** ---------------------- **/
