325613c7d082e5300e735854795149cae740e821
Andersen <andersen.ngai@stonybrook.edu>
2025-09-12 21:23:04 +0000

    Finished float.

--- src/float.c
+++ src/float.c
@@ -2,7 +2,7 @@
 #include <float.h>
 
 
-float construct_float_sf(char sign_bit, char exponent, unsigned int fraction) {
+float construct_float_sf(char sign_bit, unsigned char exponent, unsigned int fraction) {
     /* DO NOT CHANGE THE DECLARATION OF "f" (This will be converted to float later) */
     unsigned int f = 0; // DO NOT CHANGE
     /*------------------------------------------------------------------------------*/
@@ -34,22 +34,16 @@ int main(int argc, char* argv[]){
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
 
-    construct_float_sf(0x01, 0x81, 0x300000); // -5.5
+    printf("%f\n", construct_float_sf(0x00, 0x81, 0x300000)); // 5.5
+    printf("%f\n", construct_float_sf(0x01, 0x81, 0x300000)); // -5.5
+    printf("%f\n", construct_float_sf(0x00, 0x7F, 0x200000)); // 1.25
+    printf("%f\n", construct_float_sf(0x01, 0x7F, 0x200000)); // -1.25
+    printf("%f\n", construct_float_sf(0x00, 0x76, 0x299B6F)); // 0.002588
+    printf("%f\n", construct_float_sf(0x01, 0x76, 0x299B6F)); // -0.002588
+    printf("%f\n", construct_float_sf(0x00, 0x89, 0xABCDEF)); // 1374.44
+    printf("%f\n", construct_float_sf(0x01, 0x89, 0xABCDEF)); // -1374.44
+    printf("%f\n", construct_float_sf(0x00, 0x90, 0x7973C0)); // 255439
+    printf("%f\n", construct_float_sf(0x01, 0x90, 0x7973C0)); // -255439
 
-    /*
-    printf("%d\n", construct_float_sf(0x00, 0x81, 0x300000)); // 5.5
-    printf("%d\n", construct_float_sf(0x01, 0x81, 0x300000)); // -5.5
-    printf("%d\n", construct_float_sf(0x00, 0x7F, 0x200000)); // 1.25
-    printf("%d\n", construct_float_sf(0x01, 0x7F, 0x200000)); // -1.25
-    printf("%d\n", construct_float_sf(0x00, 0x76, 0x299B6F)); // 0.002588
-    printf("%d\n", construct_float_sf(0x01, 0x76, 0x299B6F)); // -0.002588
-    printf("%d\n", construct_float_sf(0x00, 0x89, 0xABCDEF)); // 1374.44
-    printf("%d\n", construct_float_sf(0x01, 0x89, 0xABCDEF)); // -1374.44
-    printf("%d\n", construct_float_sf(0x00, 0x90, 0x7973C0)); // 255439
-    printf("%d\n", construct_float_sf(0x01, 0x90, 0x7973C0)); // -255439 */
-
-	
-	/** ---------------------- **/
 	return 0;
 }
-
